0f287ae449722e304c161a6749800a4010b7f9e4
jonathan strid <pjstrid@outlook.com>
2026-04-29 09:36:25 +0100

    setup/Google services and added gitignore

---
+++ MyHabitTracker/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  MyHabitTracker
+//
+//  Created by Jonathan Strid on 2026-04-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
